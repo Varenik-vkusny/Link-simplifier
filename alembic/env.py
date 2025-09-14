382d597227603a1d74deacd71ef4ea7153ed83a8
Varenik-vkusny <akybaevtimur7@gmail.com>
2025-09-14 17:10:19 +0500

    Try to resolve git conflict

--- alembic/env.py
+++ alembic/env.py
@@ -13,6 +13,7 @@ from src.backend.config import get_settings
 
 settings = get_settings()
 
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config

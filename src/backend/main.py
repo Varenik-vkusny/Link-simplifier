1572dedba0381cdcf8f08c54355f70dd0b426a01
Varenik-vkusny <akybaevtimur7@gmail.com>
2025-09-14 16:42:01 +0500

    Add monitoring process

--- src/backend/main.py
+++ src/backend/main.py
@@ -3,6 +3,7 @@ import redis.asyncio as redis
 from fastapi import FastAPI
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from contextlib import asynccontextmanager
+from prometheus_fastapi_instrumentator import Instrumentator
 from .config import get_settings
 from .routers import users, links, redirect
 from . import client
@@ -13,37 +14,41 @@ settings = get_settings()
 
 logging.basicConfig(level=logging.INFO)
 
-scheduler = AsyncIOScheduler(timezone = 'Asia/Almaty')
+scheduler = AsyncIOScheduler(timezone="Asia/Almaty")
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    logging.info('Приложение запускается')
+    logging.info("Приложение запускается")
 
-    client.redis_client = redis.from_url(settings.redis_url, encoding='utf-8', decode_responses=True)
-    logging.info('Redis client запущен')
+    client.redis_client = redis.from_url(
+        settings.redis_url, encoding="utf-8", decode_responses=True
+    )
+    logging.info("Redis client запущен")
 
-    scheduler.add_job(sync_redis_clicks_to_db, trigger='interval', hours=1)
+    scheduler.add_job(sync_redis_clicks_to_db, trigger="interval", hours=1)
     scheduler.start()
-    logging.info('Планировщик запущен')
+    logging.info("Планировщик запущен")
 
     yield
 
     if client.redis_client:
 
-        logging.info('Останавливаю Redis client')
+        logging.info("Останавливаю Redis client")
         await client.redis_client.aclose()
 
-    logging.info('Приложение остановлено')
+    logging.info("Приложение остановлено")
 
 
 app = FastAPI(lifespan=lifespan)
+instrumentator = Instrumentator().instrument(app)
+instrumentator.expose(app)
 
 app.include_router(users.router)
 app.include_router(links.router)
 app.include_router(redirect.router)
 
 
-@app.post('/')
+@app.post("/")
 def Hello():
-    return {'message': 'Welcome to my app!'}
+    return {"message": "Welcome to my app!"}
